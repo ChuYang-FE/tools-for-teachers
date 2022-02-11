ee51f03e576c54fd137242950ee413b4be1f8092
tanchuyang <865428527@qq.com>
2022-02-11 21:56:09 +0800

    feat: add functions

--- src/views/RandomName/index.tsx
+++ src/views/RandomName/index.tsx
@@ -1,116 +1,205 @@
 import { useState, useEffect } from "react";
 
-import { Switch, Tooltip, Button, Input } from "antd";
+import {
+  Switch,
+  Tooltip,
+  Button,
+  Input,
+  Tag,
+  Space,
+  Card,
+  Badge,
+  Popover,
+  Empty,
+} from "antd";
 import {
   QuestionCircleOutlined,
   CheckOutlined,
   CloseOutlined,
+  ArrowDownOutlined,
 } from "@ant-design/icons";
 
 const { TextArea } = Input;
 
 const RandomName = () => {
-  const cacheRandomNumber = localStorage.getItem("cacheRandomNumber");
-  const cacheNeedStorage = localStorage.getItem("cacheNeedStorage");
+  const randomNameHistoryRes = localStorage.getItem("randomNameHistoryRes");
+  const randomNameNeedStorage = localStorage.getItem("randomNameNeedStorage");
+
+  const [popoverVisible, setPopoverVisible] = useState(false);
 
   const [namesArray, setNamesArray] = useState([]);
-  const [min, setMin] = useState(0);
-  const [max, setMax] = useState(0);
   const [result, setResult] = useState(0);
+
   const [needStorage, setNeedStorage] = useState(
-    cacheNeedStorage ? JSON.parse(cacheNeedStorage) : false
+    randomNameNeedStorage ? JSON.parse(randomNameNeedStorage) : false
   );
-  const [historyResult, setHistoryResult] = useState<number[]>(
-    needStorage && cacheRandomNumber ? JSON.parse(cacheRandomNumber) : []
+  const [historyResult, setHistoryResult] = useState(
+    randomNameNeedStorage && randomNameHistoryRes
+      ? JSON.parse(randomNameHistoryRes)
+      : []
   );
 
   useEffect(() => {
     console.log(namesArray);
-
-    // return () => {
-    //   second;
-    // };
   }, [namesArray]);
 
   const handleRandom = () => {
-    // the range of Math.random() is [0, 1), not include 1. So need to add 1
-    const res = Math.floor(min + (max - min + 1) * Math.random());
+    // the range of Math.random() is [0, 1)
+    const randomIndex = Math.floor(namesArray.length * Math.random());
+    const res = namesArray[randomIndex];
     setResult(res);
     const allRes = [...historyResult, res];
     setHistoryResult(allRes);
-    localStorage.setItem("cacheRandomNumber", JSON.stringify(allRes));
+    localStorage.setItem("randomNameHistoryRes", JSON.stringify(allRes));
   };
 
   const handleClear = () => {
     setHistoryResult([]);
-    localStorage.removeItem("cacheRandomNumber");
+    localStorage.removeItem("randomNameHistoryRes");
   };
 
   const handleSwitch = (checked: boolean) => {
     setNeedStorage(checked);
-    localStorage.setItem("cacheNeedStorage", JSON.stringify(checked));
+    localStorage.setItem("randomNameNeedStorage", JSON.stringify(checked));
   };
 
   const handleInputChange = (e: { target: { value: any } }) => {
-    setNamesArray(e.target.value.trim().split("\n"));
+    const finalRes = e.target.value
+      .trim()
+      .split("\n")
+      .filter((item: any) => item !== "");
+    setNamesArray(finalRes);
+  };
+
+  const saveList = () => {
+    setPopoverVisible(false);
+  };
+
+  const popoverVisibleChange = (visible: boolean) => {
+    setPopoverVisible(visible);
   };
 
   return (
-    <div>
+    <Space direction="vertical" style={{ width: "100%" }}>
       <h2>随机点名</h2>
-      <div style={{ margin: "10px 0" }}>
+      <div>
         <TextArea
           showCount
           maxLength={5000}
           autoSize={{ minRows: 5, maxRows: 10 }}
           onChange={handleInputChange}
-          style={{ margin: "10px 0" }}
+          placeholder="请输入内容，一行即一项"
         />
-        <Button
-          type="primary"
-          onClick={handleRandom}
+      </div>
+      <div>
+        <Card
+          title={
+            <div style={{ display: "flex", alignItems: "center" }}>
+              <Space>
+                <span>生成列表</span>
+                <Badge
+                  count={namesArray.length || 0}
+                  style={{ backgroundColor: "#52c41a" }}
+                />
+              </Space>
+            </div>
+          }
+          extra={
+            <Popover
+              placement="left"
+              content={
+                <Space>
+                  <Input placeholder="列表名称" />
+                  <Button type="link" onClick={saveList}>
+                    确定
+                  </Button>
+                </Space>
+              }
+              // title="列表名称"
+              trigger="click"
+              visible={popoverVisible}
+              onVisibleChange={popoverVisibleChange}
+            >
+              <Button type="default">保存</Button>
+            </Popover>
+          }
+        >
+          {namesArray.length === 0 ? (
+            <Empty />
+          ) : (
+            namesArray.map((item, index) => (
+              <Tag
+                key={`${item}-${index}`}
+                style={{ margin: 5 }}
+                color="#6773D8"
+              >
+                {item}
+              </Tag>
+            ))
+          )}
+        </Card>
+      </div>
+      <Space direction="vertical" align="center" style={{ width: "100%" }}>
+        <Button type="primary" onClick={handleRandom}>
+          抽取
+        </Button>
+        <ArrowDownOutlined />
+        <div
           style={{
-            margin: "10px 0",
+            minWidth: 180,
+            minHeight: 48,
+            padding: 2,
+            boxSizing: "border-box",
+            backgroundColor: "#aef062",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            fontSize: 24,
           }}
         >
-          开始点名
-        </Button>
-      </div>
-      <h4>本次结果：</h4>
-      <h3>{result}</h3>
-      <hr />
-      <h4>
-        历史结果：
-        <Button type="primary" danger onClick={handleClear}>
-          清空历史
-        </Button>
-      </h4>
-
-      <div style={{ margin: "10px 0" }}>
-        <span>
-          缓存
-          <Tooltip title="启用后，将缓存所有历史结果">
-            <QuestionCircleOutlined />
-          </Tooltip>
-          ：
-        </span>
-        <Switch
-          onChange={handleSwitch}
-          checked={needStorage}
-          checkedChildren={<CheckOutlined />}
-          unCheckedChildren={<CloseOutlined />}
-        />
-      </div>
-
-      <div style={{ display: "flex", flexWrap: "wrap" }}>
-        {historyResult.map((item, index) => (
-          <div key={`${index}${item}`} style={{ minWidth: 120 }}>
-            第{index + 1}次：<h3>{item}</h3>
-          </div>
-        ))}
-      </div>
-    </div>
+          {result}
+        </div>
+      </Space>
+      <Card
+        bodyStyle={{ maxHeight: 300, overflow: "auto" }}
+        title="历史结果"
+        extra={
+          <Space>
+            <span>
+              缓存
+              <Tooltip title="启用后，将缓存所有历史结果">
+                <QuestionCircleOutlined />
+              </Tooltip>
+              ：
+            </span>
+            <Switch
+              onChange={handleSwitch}
+              checked={needStorage}
+              checkedChildren={<CheckOutlined />}
+              unCheckedChildren={<CloseOutlined />}
+            />
+            <Button type="primary" danger onClick={handleClear}>
+              清空历史
+            </Button>
+          </Space>
+        }
+      >
+        {historyResult.length ? (
+          <Space wrap>
+            {historyResult.map((item: any, index: number) => (
+              <div key={`${index}${item}`} style={{ minWidth: 120 }}>
+                第{index + 1}次：<h3>{item}</h3>
+              </div>
+            ))}
+          </Space>
+        ) : (
+          <Empty />
+        )}
+      </Card>
+    </Space>
   );
 };
+
+
 
 export default RandomName;
